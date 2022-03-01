99d09fa686e110ccece0ce3738e0513a511bb3c7
user <user@DESKTOP-D5CE1IK>
2022-03-01 20:44:07 +0530

    welcome

---
+++ grouyp1/src/main/java/grouyp1/Demo_1.java
@@ -0,0 +1,8 @@
+package grouyp1;
+
+public class Demo_1 {
+	public static void main(String[] args)
+	{
+		System.out.println("Whatsupp");
+	}
+}
---
+++ grouyp1/src/main/java/grouyp1/demo.java
@@ -0,0 +1,9 @@
+package grouyp1;
+
+public class demo {
+	public static void main(String []args)
+	 {
+		 System.out.println("Welcome");
+	 }
+
+}
